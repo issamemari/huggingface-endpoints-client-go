27df55a6dcee356e95900225ec2aab7e9a13a8d0
Issa Memari <issa.memari@gmail.com>
2024-06-02 21:28:32 +0200

    Better error formatting

--- client.go
+++ client.go
@@ -1,6 +1,10 @@
 package huggingface
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
@@ -32,4 +36,53 @@ func NewClient(host, namespace, token *string) (*Client, error) {
 	c.Namespace = *namespace
 
 	return &c, nil
+}
+
+func (c *Client) DoRequest(method, path string, body interface{}) ([]byte, error) {
+	var reqBody *bytes.Buffer
+	var reqBodyJSON string
+	if body != nil {
+		reqBody = new(bytes.Buffer)
+		err := json.NewEncoder(reqBody).Encode(body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to encode request body: %w", err)
+		}
+		reqBodyJSON = reqBody.String()
+	} else {
+		reqBody = bytes.NewBuffer([]byte{})
+		reqBodyJSON = "{}"
+	}
+
+	url := c.HostURL
+	if c.Namespace != "" {
+		url = fmt.Sprintf("%s/%s", url, c.Namespace)
+	}
+	if path != "" {
+		url = fmt.Sprintf("%s/%s", url, path)
+	}
+
+	req, err := http.NewRequest(method, url, reqBody)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create new request: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.Token))
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	respBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d, url: %s, request body: %s, response body: %s", res.StatusCode, url, reqBodyJSON, respBody)
+	}
+
+	return respBody, nil
 }
--- endpoints.go
+++ endpoints.go
@@ -1,58 +1,9 @@
 package huggingface
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 )
-
-func (c *Client) DoRequest(method, path string, body interface{}) ([]byte, error) {
-	var reqBody *bytes.Buffer
-	if body != nil {
-		reqBody = new(bytes.Buffer)
-		err := json.NewEncoder(reqBody).Encode(body)
-		if err != nil {
-			return nil, fmt.Errorf("failed to encode request body: %w", err)
-		}
-	} else {
-		reqBody = bytes.NewBuffer([]byte{})
-	}
-
-	url := c.HostURL
-	if c.Namespace != "" {
-		url = fmt.Sprintf("%s/%s", url, c.Namespace)
-	}
-	if path != "" {
-		url = fmt.Sprintf("%s/%s", url, path)
-	}
-
-	req, err := http.NewRequest(method, url, reqBody)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create new request: %w", err)
-	}
-
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.Token))
-	req.Header.Set("Content-Type", "application/json")
-
-	res, err := c.HTTPClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
-
-	respBody, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("status: %d, url: %s, body: %s", res.StatusCode, url, body)
-	}
-
-	return respBody, err
-}
 
 func (c *Client) ListEndpoints() ([]Endpoint, error) {
 	body, err := c.DoRequest("GET", "", nil)
